a77fd65d3386ec7f7ad2204a82d07f496e5af280
Laís Boscolo <costaboscololais2008@gmail.com>
2025-04-28 11:22:23 -0300

    att

---
+++ interfaces/Locavel.php
@@ -0,0 +1,11 @@
+<?php
+namespace Interfaces;
+
+// Interface que define os métodos necessários para um veículo ser locável
+
+interface Locavel {
+    public function alugar() : string;
+    public function devolver() : string;
+    public function isDisponivel() : bool;
+}
+?>
---
+++ models/Veiculo.php
@@ -0,0 +1,35 @@
+<?php
+namespace Models;
+
+// Classe abstrata para todos os tipos de veiculos
+
+abstract class Veiculo {
+    protected string $modelo;
+    protected string $placa;
+    protected bool $disponivel;
+
+    public function __construct (string $modelo, string $placa){
+        $this -> modelo = $modelo;
+        $this -> placa = $placa;
+        $this -> disponivel = true;
+    }
+
+    //  Função para cálculo de aluguel / float pq e valor de dinheiro
+    abstract public function calcularAluguel(int $dias) : float;
+
+    public function isDisponivel(): bool {
+        return $this->disponivel;
+    }
+    public function getModelo(): string{
+        return $this->modelo;
+    }
+    public function getPlaca(): string{
+        return $this->placa;
+    }
+    public function setDisponivel (bool $disponivel):void{
+        // void = vazio
+        
+    }
+
+}
+
