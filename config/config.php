c92bac21a5d5e4c2944c12a1dc9f06f8a0429907
Laís Boscolo <costaboscololais2008@gmail.com>
2025-04-28 10:08:21 -0300

    att

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+// Arquivo de configuraçâo com as constantes do sistema
+
+define('ARQUIVO_JSON', __DIR__ . '/../data/veiculos.json');
+define('ARQUIVO_USUARIOS', __DIR__ . '/../data/usuarios.json');
